7d45c47f72194761bfe6382da63206e1f72d71d1
Anshika <anshika2020ag@gmail.com>
2024-07-22 22:31:25 +0530

    series

--- OneDrive/Desktop/python/pandas/1.py
+++ OneDrive/Desktop/python/pandas/1.py
@@ -2,3 +2,9 @@ import pandas as pd
 arr = [1,2,3]
 myvar = pd.Series(arr)
 print(myvar)
+
+ns = pd.Series(arr, index=["index1", "index2", "index3"])
+print(ns)  
+
+s = [(1,2), (4,5,6)]
+print(pd.Series(s))
